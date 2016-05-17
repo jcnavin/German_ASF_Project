6611e2a0f9838ab1043891e42a6cf7b6304e7b68
anderaa <aaronanderson.co@gmail.com>
2016-05-17 09:42:43 -0600

    layout the R script

---
+++ ASF_Model_V1.R
@@ -0,0 +1,59 @@
+
+###############################################################################
+# Model for ASF in German wild boar
+#
+# Formatting notes:
+#   - use camel case, no periods or underscores when naming objects
+#   - all objects begin with lower case letter, except use uppercase for first 
+#     letter of function names
+#   - use descriptive names for objects
+#     e.g. 'mortalityProb' rather than 'mp'
+#   - indent should be two spaces
+#   - use spaces around mathematical operators, except when specifying function 
+#     arguments
+#   - reference columns of any matrix by name rather than index number
+#   - wrap lines of code longer than 79 characters (width of the # bars)
+###############################################################################
+
+
+
+rm(list=ls())
+set.seed(1)
+
+
+
+###############################################################################
+# Input Section
+
+# First line of inputs here
+
+###############################################################################
+
+
+
+###############################################################################
+# Functions
+
+#InitialPopulation <- function() {
+#  # this functions returns an initial popMatrix
+#
+#  
+#  return(popMatrix)
+#}
+
+###############################################################################
+
+
+
+###############################################################################
+# Loops
+
+#popMatrix <- InitialPopulation()
+
+#for(d in 1:365) {
+#  popMatrix <- Mortality()
+#  popMatrix <- Reproduction()
+#}
+
+
+###############################################################################
