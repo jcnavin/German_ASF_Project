2fcebe7d28631e7d4da2e8f64803da90ad9833a4
anderaa <aaronanderson.co@gmail.com>
2016-05-20 16:02:06 -0600

    sounder and cell assignment now on, need to check

--- ASF_Model_V1.R
+++ ASF_Model_V1.R
@@ -35,17 +35,26 @@ set.seed(1)
 # Input Section
 
 # inputs for init population
-initAbundPerCell  <- 200
-numberCells       <- 61
+initAbundPerCell  <- 100
+numberCells       <- 2
 initAdultFemales  <- round(.25 * initAbundPerCell)
 initPigletFemales <- round(.24 * initAbundPerCell)
 initAdultMales    <- round(.21 * initAbundPerCell)
 initPigletMales   <- initAbundPerCell - 
                      sum(initAdultFemales + initPigletFemales + initAdultMales)
+adultFemStart   <- 1
+adultFemEnd     <- initAdultFemales
+pigletFemStart  <- initAdultFemales + 1
+pigletFemEnd    <- initAdultFemales + initPigletFemales
+adultMaleStart  <- initAdultFemales + initPigletFemales + 1
+adultMaleEnd    <- initAdultFemales + initPigletFemales + initAdultMales
+pigletMaleStart <- initAdultFemales + initPigletFemales + initAdultMales + 1
+pigletMaleEnd   <- initAbundPerCell
+
 initPigletsPerSounder <- 15
 
 # inputs for sounders
-maxFemalesPerSounder <- 7
+maxFemalesPerSounder <- 3
 maxPigletAge <- 10 * 30
 
 traitList <- c( 'id', 'sounderId', 'cell', 'age', 'female')
@@ -64,57 +73,51 @@ traitList <- c( 'id', 'sounderId', 'cell', 'age', 'female')
 
 ###############################################################################
 # Functions
+
+  # create popMatrix
   popMatrix <- matrix(0, nrow=initAbundPerCell * numberCells, 
                       ncol=length(traitList))
   colnames(popMatrix) <- traitList
+  
+  idFill <- 1
 
-  # create empty matrix
   for(k in 1:numberCells) {
+    # create location specific matrix
     celMatrix <- matrix(0, nrow=initAbundPerCell, ncol=length(traitList))
     colnames(celMatrix) <- traitList
     celMatrix[, 'cell'] <- k
 
     # assign id and sex numbers
     celMatrix[, 'id'] <- seq(1:nrow(celMatrix))
-    celMatrix[1:(initAdultFemales + initPigletFemales), 'female'] <- 1
-
+    celMatrix[adultFemStart:pigletFemEnd, 'female'] <- 1
+    
     # assign age to adult females
-    celMatrix[1:initAdultFemales, 'age'] <-
+    celMatrix[adultFemStart:adultFemEnd, 'age'] <-
       round(rtriangle(initAdultFemales, a=(19*30), b=(96*30), c=(19*30)))
 
     # assign age to piglet females
     ageSeq <- c(rep(sample((30*7):(maxPigletAge), 
                           initPigletFemales%/%3, replace=TRUE), 3),
                 rep(sample((30*7):(maxPigletAge), 1), initPigletFemales%%3))
-    celMatrix[(initAdultFemales+1):
-              (initAdultFemales+initPigletFemales),'age'] <- ageSeq
+    celMatrix[pigletFemStart:pigletFemEnd,'age'] <- ageSeq
   
     # assign age to adult males
-    celMatrix[(initAdultFemales+initPigletFemales + 1):
-              (initAbundPerCell-initPigletMales), 'age'] <- 
+    celMatrix[adultMaleStart:adultMaleEnd, 'age'] <- 
       round(rtriangle(initAdultMales, a=(19*30), b=(72*30), c=(19*30)))
   
     # assign ages to piglet males
     ageSeq <- c(rep(sample((30*7):(maxPigletAge), 
                           initPigletMales%/%3, replace=TRUE), 3),
                 rep(sample((30*7):(maxPigletAge), 1), initPigletMales%%3))
-    celMatrix[(initAbundPerCell-initPigletMales+1):
-              initAbundPerCell, 'age']<- ageSeq  
-
-    # assign sounder id's to adult solo males
-    soloMales <- sum(celMatrix[, 'female'] == 0 & 
-                     celMatrix[, 'age'] > (18*30))
-    celMatrix[celMatrix[, 'female'] == 0 & 
-              celMatrix[, 'age'] > (18*30), 'sounderId'] <- seq(1, soloMales)
+    celMatrix[pigletMaleStart:pigletMaleEnd, 'age']<- ageSeq  
 
     # assign sounder id's to adult females
     # JORDAN: I replaced your loop with this. Sorry!
     for(i in 1:initAdultFemales) {
-      celMatrix[i, 'sounderId'] <- soloMales + (i-1) %/% maxFemalesPerSounder
+      celMatrix[i, 'sounderId'] <- idFill + (i-1) %/% maxFemalesPerSounder
     }
 
     # assign sounder ids' to piglets
-    idFill           <- soloMales + 1
     pigletFemRow     <- initAdultFemales + 1
     pigletMaleRow    <- initAdultFemales + initPigletFemales + initAdultMales + 1 
     pigletFemEndRow  <- initAdultFemales + initPigletFemales
@@ -135,16 +138,26 @@ traitList <- c( 'id', 'sounderId', 'cell', 'age', 'female')
           if (draw > 0.5) {
             celMatrix[, 'sounderId'][f]  <- idFill
             f <- min(f + 1, pigletFemEndRow)
+            if (f == pigletFemEndRow) {
+              outOfFemalePiglets <- 1
+            }
           } else {
             celMatrix[, 'sounderId'][m] <- idFill
             m <- min(m + 1, pigletMaleEndRow)
+            if (m == pigletMaleEndRow) {
+              outOfMalePiglets <- 1
+            }
           }
-        } else { 
-          celMatrix[, 'sounderId'][f] <- idFill
-          celMatrix[, 'sounderId'][m] <- idFill
+        } else {
+          if(outOfFemalePiglets == 0) {
+            celMatrix[, 'sounderId'][f] <- idFill
+          }
+          if(outOfMalePiglets == 0) {
+            celMatrix[, 'sounderId'][m] <- idFill
+          }
           spotsRemaining <- initPigletsPerSounder - 
                             sum(celMatrix[, 'sounderId'] == idFill &
-                                celMatrix[, 'age'] < (10 * 30))
+                                celMatrix[, 'age'] <= (10 * 30))
           if (m == pigletMaleEndRow) {
             outOfMalePiglets <- 1
           }
@@ -156,15 +169,28 @@ traitList <- c( 'id', 'sounderId', 'cell', 'age', 'female')
           }
           m <- min(m + 1, pigletMaleEndRow)
           f <- min(f + 1, pigletFemEndRow)
-        }
+        } # close else
       }  # close inner while loop
       spotsRemaining <- initPigletsPerSounder 
       idFill <- idFill + 1
       stop <- 0
     }  # close outer while loop
+    
+    # assign sounder id's to adult solo males
+    idFill <- 1 + max(celMatrix[, 'sounderId'])
+    soloMales <- sum(celMatrix[, 'female'] == 0 & 
+                       celMatrix[, 'age'] > (18*30))
+    celMatrix[celMatrix[, 'female'] == 0 & 
+                celMatrix[, 'age'] > (18*30), 'sounderId'] <- 
+      seq(idFill, (idFill + soloMales - 1))
+    idFill <- idFill + soloMales
+    
+    # put celMatrix into its place in popMatrix
     popMatrix[(initAbundPerCell * (k - 1) + 1):
               (initAbundPerCell * k), ] <- celMatrix
   }  # close for loop
+  
+ 
 
 ###############################################################################
 
